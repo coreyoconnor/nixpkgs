6a77d7af02167016888d13ffde05b2a923c047d8
Corey O'Connor <coreyoconnor@gmail.com>
2023-08-23 19:46:32 -0700

    fixes for raspberry pi cm 3+

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11565,6 +11565,7 @@ with pkgs;
     ubootRaspberryPi3_64bit
     ubootRaspberryPi4_32bit
     ubootRaspberryPi4_64bit
+    ubootRaspberryPiGeneric
     ubootRaspberryPiZero
     ubootRock4CPlus
     ubootRock5ModelB
